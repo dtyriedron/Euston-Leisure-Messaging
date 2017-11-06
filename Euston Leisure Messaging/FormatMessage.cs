b35e9a3ae0e5c8b16b7ff3ec9d2cd1ef788d1613
Dylan <dman9887@gmail.com>
2017-11-06 21:06:07 +0000

    got hashtags stored and goit the twitterid stored still getting exceptions for the storing of the hashtags but its k i caught them

--- Euston Leisure Messaging/FormatMessage.cs
+++ Euston Leisure Messaging/FormatMessage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Euston_Leisure_Messaging
@@ -13,6 +14,12 @@ namespace Euston_Leisure_Messaging
         public Body body;
         private Message message;
         public String messageText;
+        //public Regex phoneregex = new Regex(@"(?<=+)\w+");
+        //public Regex emailregex = new Regex(@"(?<=@>=?)\w+");
+        public Regex hashregex = new Regex(@"(?<=#)\w+");
+
+        private static string[] matches = new string[60];
+        public static string[] Matches { get => matches; set => matches = value; }
 
         internal Message Message { get => message; set => message = value; }
 
@@ -41,32 +48,29 @@ namespace Euston_Leisure_Messaging
             return Type.NULL;
         }
 
-        public static String getBetween(String text, Char strStart, Char strEnd)
+        public string[] findRegex(string text, Regex regex)
         {
-            int start, end;
-            if (text.Contains(strStart) && text.Contains(strEnd))
+            var matches = regex.Matches(text);
+            int index = 0;
+            foreach (Match m in matches)
             {
-                start = text.IndexOf(strStart, 0);
-                end = text.IndexOf(strEnd, start);
-                return text.Substring(start, end - start);
+                Matches[index] = m.Value;
+                Console.WriteLine("added match:   " + Matches[index]);
+                index++;
             }
-            else
-            {
-                return "";
-            }
+            return Matches;
         }
-
 
         public void MessageHandler()
         {
             if (message.Type.Equals(Type.SMS))
             {
-                String phoneNum = getBetween(messageText, '+', ' ');
+                //String phoneNum = getBetween(messageText, '+', ' ');
                 String textMessage = messageText.Substring(0, Math.Min(140, messageText.Length));
             }
             else if (message.Type.Equals(Type.Email))
             {
-                String Email = getBetween(messageText, '@', '.');
+                //String Email = getBetween(messageText, '@', '.');
                 //put in a new line worth 20 characters for the subject
                 String emailMessage = messageText.Substring(0, Math.Min(1028, messageText.Length));
             }
@@ -74,30 +78,33 @@ namespace Euston_Leisure_Messaging
             {
                 String tweetMessage = messageText.Substring(0, Math.Min(140, messageText.Length));
                 String twitterid = tweetMessage.Substring(0, Math.Min(16, tweetMessage.Length));
-                twitterid = getBetween(tweetMessage, '@', ' ');
-                int index = 0;
-
-                if (tweetMessage.Contains('#'))
+                twitterid = findRegex(tweetMessage, new Regex(@"(?<=@)\w+"))[0];
+                Console.WriteLine(twitterid);
+                //store the hashtag and its count in an array if its already there then count++                
+                foreach (var hashtag in findRegex(tweetMessage, hashregex))
                 {
-                    //store the hashtag and its count in an array if its already there then count++
-                    Test.IsHashtag[index] = getBetween(tweetMessage, '#', ' ');
-                    Console.WriteLine(" HEEEEELLELELLLLLLLOOOOO:   "+Test.IsHashtag[index]);
-                    foreach (var hashtag in Test.IsHashtag)
-                    {
-                       if (!Test.Hashtags.ContainsKey(hashtag))
-                       {
-                           Test.Hashtags.Add(hashtag, 1);
-                       }
-                        else
-                       {
-                           Test.Hashtags[hashtag]++;
-                           Console.WriteLine(Test.Hashtags[hashtag]);
-                       }
-                            
-                   }
-                   index++;
-                }
+                      Console.WriteLine("foreach");
 
+                    try
+                    {
+                        if (Test.Hashtags.ContainsKey(hashtag))
+                        {
+                            Console.WriteLine("adding to dictionary count");
+                            Test.Hashtags.TryGetValue(hashtag, out int val);
+                            Test.Hashtags[hashtag] = val + 1;
+                            Console.WriteLine(Test.Hashtags[hashtag]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("adding to dictionary");
+                            Test.Hashtags.Add(hashtag, 1);
+                            Console.WriteLine("added to the dictionary");
+                        }
+                    }
+                    catch (Exception ex)
+                    {}
+                            
+                }
             }
         }
     }
